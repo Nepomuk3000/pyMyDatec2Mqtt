8c0dea56d0f84b3176d6463601c9d305dc21b37f
Michaël Luitaud <ew11@luitaud.fr>
2024-02-08 07:18:00 +0100

    Utilisation du path absolu du fichier config.json

--- MyDatecData.py
+++ MyDatecData.py
@@ -1,9 +1,11 @@
+import os
 import json
 
 class MyDatecDataClass:
     instance = None
     data = {}
-    
+    path = os.path.dirname(os.path.abspath(__file__))
+
     def __new__(cls):
         if cls.instance is None:
             cls.instance = super(MyDatecDataClass, cls).__new__(cls)
@@ -17,11 +19,11 @@ class MyDatecDataClass:
         self.data[key] = value
         
     def load(self):
-        with open('config/config.json', 'r') as jsonfile:
+        with open(self.path + '/config/config.json', 'r') as jsonfile:
             self.data = json.load(jsonfile)
         
     def save(self):
-        with open('config/config.json', 'w') as jsonfile:
+        with open(self.path + '/config/config.json', 'w') as jsonfile:
             json.dump(MyDatecData.data, jsonfile, indent=2)
         
 MyDatecData = MyDatecDataClass()
